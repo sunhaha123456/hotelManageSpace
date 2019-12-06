7ebe8a9649b1a87d537401a98a3721162b889de4
Administrator <771968690@qq.com>
2019-12-06 12:49:20 +0800

    sunpeng

---
+++ rose-controler/src/main/java/com/rose/controler/EmployerSalaryHistoryControler.java
@@ -0,0 +1,40 @@
+package com.rose.controler;
+
+import com.rose.common.data.base.PageList;
+import com.rose.common.data.response.ResponseResultCode;
+import com.rose.common.exception.BusinessException;
+import com.rose.common.util.StringUtil;
+import com.rose.data.entity.TbEmployer;
+import com.rose.data.to.request.EmployerSearchRequest;
+import com.rose.service.EmployerManageService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import javax.inject.Inject;
+
+/**
+ * 功能：发薪流水 controller
+ * @author sunpeng
+ * @date 2019
+ */
+@Slf4j
+@RestController
+@RequestMapping("/user/employerSalaryHistory")
+public class EmployerSalaryHistoryControler {
+
+    @Inject
+    private EmployerManageService employerManageService;
+
+    @PostMapping(value= "/search")
+    public PageList<TbEmployer> search(@RequestBody EmployerSearchRequest param) throws Exception {
+        if (param == null) {
+            throw new BusinessException(ResponseResultCode.PARAM_ERROR);
+        }
+        return employerManageService.searchEmployer(param);
+    }
+
+    @GetMapping(value= "/delete")
+    public void delete(@RequestParam Long id) throws Exception {
+        employerManageService.deleteEmployer(id);
+    }
+}
